291680fc177ba180220374c29d3dc25058d7a5bc
João Pinto <joao.pinto@blip.pt>
2015-03-31 11:03:00 +0100

    new github url

--- tftpboot/real_boot_script.php
+++ tftpboot/real_boot_script.php
@@ -1,21 +1,7 @@
 #!ipxe
 
-kernel http://centos.mirror.ptisp.pt/centos/6.6/os/x86_64/isolinux/vmlinuz bootfile=http://boot.ipxe.org/demo/boot.php fastboot initrd=http://centos.mirror.ptisp.pt/centos/6.6/os/x86_64/isolinux/initrd.img
+kernel http://centos.mirror.ptisp.pt/centos/6.6/os/x86_64/isolinux/vmlinuz bootfile=https://raw.githubusercontent.com/pintoj/pepsi-challenge/master/tftpboot/real_boot_script.php fastboot initrd=http://centos.mirror.ptisp.pt/centos/6.6/os/x86_64/isolinux/initrd.img
 initrd http://centos.mirror.ptisp.pt/centos/6.6/os/x86_64/isolinux/initrd.img ks=https://raw.githubusercontent.com/pintoj/pepsi-challenge/master/tftpboot/ks/ks6.cfg
 boot
 
-#LABEL reboot
-#    COM32 reboot.c32
-#
-#LABEL centos6
-#    MENU LABEL ^CentOS 6 From mirrors and Kickstart
-#    MENU DEFAULT
-#    LINUX http://centos.mirror.ptisp.pt/centos/6.6/os/x86_64/isolinux/vmlinuz
-#    APPEND initrd=http://centos.mirror.ptisp.pt/centos/6.6/os/x86_64/isolinux/initrd.img ks=http://172.17.200.10/anaconda-ks.cfg ramdisk_size=10000
-#
-#LABEL centos5
-#    MENU LABEL ^CentOS 5 From mirrors and Kickstart
-#    MENU DEFAULT
-#    LINUX http://centos.mirror.ptisp.pt/centos/5.11/os/x86_64/isolinux/vmlinuz
-#    APPEND initrd=http://centos.mirror.ptisp.pt/centos/5.11/os/x86_64/isolinux/initrd.img ks=http://172.17.200.10/ks5.cfg ramdisk_size=10000
 
