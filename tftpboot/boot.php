6941502257603fbf1acb3ce63a0df4a6ec696e09
João Pinto <joao.pinto@blip.pt>
2015-04-01 11:47:07 +0100

    added centos5

--- tftpboot/boot.php
+++ tftpboot/boot.php
@@ -9,6 +9,9 @@ item reboot     Reboot
 choose target && goto ${target}
 
 :centos5
+kernel http://centos.mirror.ptisp.pt/centos/5.11/os/x86_64/images/pxeboot/vmlinuz unsupported_hardware text NOVERIFYSSL ks=https://raw.githubusercontent.com/pintoj/pepsi-challenge/master/tftpboot/ks/ks5.cfg ksdevice=bootif ip=dhcp
+initrd http://centos.mirror.ptisp.pt/centos/5.11/os/x86_64/images/pxeboot/initrd.img
+boot
 
 :centos6
 kernel http://centos.mirror.ptisp.pt/centos/6.6/os/x86_64/isolinux/vmlinuz 
